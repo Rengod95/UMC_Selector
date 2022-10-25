d7586652bab2965e98bca0097e50f9a451140366
KJBig <kjgi73k@gmail.com>
2022-10-25 20:08:44 +0900

    main infinite scroll

--- server/routes/index.js
+++ server/routes/index.js
@@ -39,7 +39,7 @@ router.post('/login', async (req, res, next) => {   // 로그인
             const token = createJwtToken(req.body.userId);
             res.status(200).json({ token, result: "로그인 성공" });
 
-        } else { res.status(400).json("로그인 실패!")}
+        } else { res.status(400).json({result: "로그인 실패!"})}
 
     } catch (err) {
         console.error(err);
@@ -59,7 +59,7 @@ router.post('/register/reg', async (req, res, next) => {
         if (created) {
             const token = createJwtToken(req.body.userId);
             res.status(201).json({ token, result: "회원가입 성공" });
-        } else { res.status(400).json("회원가입 실패")}
+        } else { res.status(400).json({result:"회원가입 실패"})}
 
     } catch (err) {
         console.error(err);
@@ -104,12 +104,33 @@ router.get('/me', isAuth, (req, res) => {
 
 
 // main/에서 get keyword 필요한지
-router.get('/main', isAuth, async (req, res, next) => {
+router.get('/main?*', isAuth, async (req, res, next) => {
+    const keywordNum = 10;
+    let pageNum = req.query.page;
+    let _offset = 0;
+    if (pageNum > 1) {
+        _offset = keywordNum * (pageNum - 1);
+    }
+    let _isLast = false;
     try {
-        console.log(`nickname: ${req.nickname}, userId: ${req.userId}, partNumber: ${req.partNumber}`);
-        const allKeywords = await Keyword.findAll({where:{partNumber:req.partNumber}})
+        let keywords = null;
+            await Keyword.findAndCountAll({
+            where:{partNumber:req.partNumber},
+                offset: _offset, //req.body.pageParam,
+                limit: keywordNum,
+        })
+            .then((result) =>{
+                const a = _offset + keywordNum;
+                if(result.count <= a){
+                    console.log(`${result.count}, ${a}`);
+                    _isLast = true;
+                }
+               keywords = result.rows;
+            });
 
-        res.status(200).json({allKeywords, result:"Keyword load success."});
+
+        res.status(200).json({keywords, isLast: _isLast, result: "키워드 로드 성공"});
+
 
     } catch (err) {
         console.error(err);
@@ -122,16 +143,16 @@ router.post('/main/select', isAuth, async (req, res, next) => {
     try {
         // -> 이 부분 유저 nickName만 넣을지 id->누가 선택했는지 구분할라고...
 
-        const allKeywords = await Keyword.findOne({where:{keywordNumber: req.body.keywordNumber}})
+        const keywords = await Keyword.findOne({where:{keywordNumber: req.body.keywordNumber}})
 
         // 선택된 keyword의 selector가 비어있다면 현재 user로 채우기
-        if(allKeywords.selector === null) {
+        if(keywords.selector === null) {
             await Keyword.update({selector: req.nickname}, {where: {keywordNumber: req.body.keywordNumber}});
 
-            res.status(200).json("Select success.");
+            res.status(200).json({result: "선택 성공"});
 
         }else{ // 비어있지 않다면 error 발생
-            res.status(400).json("Keyword already selected.");
+            res.status(400).json({result:"키워드가 이미 선택되어 있음"});
         }
     } catch (err) {
         console.error(err);
@@ -144,16 +165,16 @@ router.post('/main/select', isAuth, async (req, res, next) => {
 router.put('/main/drop', isAuth, async (req, res, next) => {
     try {
 
-        const allKeywords = await Keyword.findOne({where:{keywordNumber: req.body.keywordNumber}})
+        const keywords = await Keyword.findOne({where:{keywordNumber: req.body.keywordNumber}})
 
         //선택된
-        if(allKeywords.selector !== null) {
+        if(keywords.selector !== null) {
             await Keyword.update( {selector: null},{where: { keywordNumber: req.body.keywordNumber}});
 
-            res.status(200).json({result:"Drop success."});
+            res.status(200).json({result:"드랍 성공"});
 
         }else{
-            res.status(400).json("Keyword does not select.");
+            res.status(400).json({result:"키워드가 선택되지 않음"});
         }
     } catch (err) {
         console.error(err);
@@ -167,18 +188,18 @@ router.post('/main/random', isAuth, async (req, res, next) => {
     try {
         //선택한 keyword의 selector와 현재 유저 비교후 드랍
 
-        const allKeywords = await Keyword.findAll({where: {selector: null}});
-        const len = Object.values(allKeywords).length;
+        const keywords = await Keyword.findAll({where: {selector: null}});
+        const len = Object.values(keywords).length;
         const target = Math.floor(Math.random() * len)
 
-        if(Object.keys(allKeywords).length !== 0) { //selector=null인 값이 있으면.
-            const targetId = Object.values(allKeywords)[target].keywordNumber;
+        if(Object.keys(keywords).length !== 0) { //selector=null인 값이 있으면.
+            const targetId = Object.values(keywords)[target].keywordNumber;
 
             await Keyword.update( {selector: req.nickname},{where: { keywordNumber: targetId }});
 
-            res.status(200).json("Random select success.");
+            res.status(200).json({result:"랜덤 선택 성공"});
         }else{
-            res.status(400).json("All keyword already selected.");
+            res.status(400).json({result:"이미 모든 키워드가 선택됨"});
         }
     } catch (err) {
         console.error(err);
